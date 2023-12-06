d6706e9a70dc61078594c2cf80bf88d659ff5c49
jinj <jinj@mit.edu>
2023-12-05 23:30:15 -0500

    added playernode

---
+++ project_code/voxel-game/PlayerNode.hpp
@@ -0,0 +1,28 @@
+#ifndef PLAYER_NODE_H_
+#define PLAYER_NODE_H_
+
+#include "gloo/SceneNode.hpp"
+
+namespace GLOO {
+class CameraComponent;
+
+class PlayerNode : public SceneNode {
+ public:
+  PlayerNode(float fov = 45.f, float aspect = 0.75f, float speed = 2.0f, float distance = 5.f);
+  void Update(double delta_time) override;
+
+ private:
+  void UpdateViewport();
+  void PlayerRotation(glm::dvec2 pos);
+  void PlaneTranslation(glm::dvec2 pos);
+  float fov_;
+  float speed_;
+  float distance_;
+  glm::vec3 start_position_;
+  glm::quat start_rotation_;
+  float start_distance_;
+  glm::dvec2 mouse_start_click_;
+};
+}  // namespace GLOO
+
+#endif
